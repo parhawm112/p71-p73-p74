df7469f9b5d777a121ad29b038d7e353aa155cb2
parhawm112 <117488273+parhawm112@users.noreply.github.com>
2023-01-14 12:24:13 -0800

    Add files via upload

---
+++ p71.main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	  int n,m,i,f;
+	  
+	  cout<<"plaese enter n:";
+	  cin>>n;
+	  cout<<"please enter m:";
+	  cin>>m;
+	  
+	  f=1;
+	  
+	  for(i=1;i,=m;i++){
+	  	f*=n;
+	  }
+	  cout<<f;
+}
